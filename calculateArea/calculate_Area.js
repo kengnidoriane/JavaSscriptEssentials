4a781a47c1457be7dc06f1fe3ec86e65eab58ee4
kengnidoriane <dorianekengni@gmail.com>
2025-01-24 07:58:02 -0500

    lab de declaration de fonction

---
+++ calculateArea/calculate_Area.js
@@ -0,0 +1,8 @@
+let length;
+let width;
+function calculateArea() {
+    length = parseFloat(document.getElementById('length').value);
+    width = pareFloat(document.getElementById('width').value);
+    let area = length * width;
+    document.getElementById('result').innerText = `The area of the rectangle is: ${area}`;
+}
