31f4029f2d165c6a5a7bdfddd476768733da8ab0
kengnidoriane <dorianekengni@gmail.com>
2025-01-24 08:21:10 -0500

    rectification des erreurs sur le parsefloat et creation de la fonction groceryTracker

--- calculateArea/calculate_Area.js
+++ calculateArea/calculate_Area.js
@@ -2,7 +2,20 @@ let length;
 let width;
 function calculateArea() {
     length = parseFloat(document.getElementById('length').value);
-    width = pareFloat(document.getElementById('width').value);
+    width = parseFloat(document.getElementById('width').value);
     let area = length * width;
     document.getElementById('result').innerText = `The area of the rectangle is: ${area}`;
+}
+
+let groceryAmount1;
+let groceryAmount2;
+let groceryAmount3;
+
+function groceryTracker() {
+    groceryAmount1 = parseFloat(document.getElementById('grocery1').value);
+    groceryAmount2 = parseFloat(document.getElementById('grocery2').value);
+    groceryAmount3 = parseFloat(document.getElementById('grocery3').value);
+    let totalAmount = groceryAmount1 + groceryAmount2+ groceryAmount3;
+    document.getElementById('resultTracker').innerText = `The total amount is: $ ${totalAmount}`;
+
 }
