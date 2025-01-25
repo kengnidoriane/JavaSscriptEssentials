a430890916c50aefe16871892fba653b89b9456c
kengnidoriane <dorianekengni@gmail.com>
2025-01-25 05:56:55 -0500

    ajout du bouton de recherche d employe par specialisation specifique

--- employeeDetails/employee_details.js
+++ employeeDetails/employee_details.js
@@ -1,21 +1,21 @@
 const employees = [
-    { id: 1, name: 'John Doe', age: 30, department: 'IT', salary: 50000 },
-    { id: 2, name: 'Alice Smith', age: 28, department: 'HR', salary: 45000 },
-    { id: 3, name: 'Bob Johnson', age: 35, department: 'Finance', salary: 60000 },
-    { id: 4, name: 'Emily Davis', age: 32, department: 'Marketing', salary: 55000 },
-    { id: 5, name: 'Michael Brown', age: 40, department: 'IT', salary: 70000 },
-    { id: 6, name: 'Sarah Wilson', age: 29, department: 'HR', salary: 48000 },
-    { id: 7, name: 'David Miller', age: 45, department: 'Finance', salary: 75000 },
-    { id: 8, name: 'Sophia Taylor', age: 27, department: 'Marketing', salary: 52000 },
-    { id: 9, name: 'James Anderson', age: 38, department: 'IT', salary: 62000 },
-    { id: 10, name: 'Olivia Thomas', age: 31, department: 'Sales', salary: 53000 },
+    { id: 1, name: 'John Doe', age: 30, department: 'IT', salary: 50000, specialization: 'Java' },
+    { id: 2, name: 'Alice Smith', age: 28, department: 'HR', salary: 45000, specialization: 'JavaScript' },
+    { id: 3, name: 'Bob Johnson', age: 35, department: 'Finance', salary: 60000, specialization: 'ReactJs' },
+    { id: 4, name: 'Emily Davis', age: 32, department: 'Marketing', salary: 55000, specialization: 'NodeJs' },
+    { id: 5, name: 'Michael Brown', age: 40, department: 'IT', salary: 70000, specialization: 'Python' },
+    { id: 6, name: 'Sarah Wilson', age: 29, department: 'HR', salary: 48000, specialization: 'JavaScript' },
+    { id: 7, name: 'David Miller', age: 45, department: 'Finance', salary: 75000, specialization: 'Kotlin' },
+    { id: 8, name: 'Sophia Taylor', age: 27, department: 'Marketing', salary: 52000, specialization: 'Swift' },
+    { id: 9, name: 'James Anderson', age: 38, department: 'IT', salary: 62000, specialization: 'JavaScript' },
+    { id: 10, name: 'Olivia Thomas', age: 31, department: 'Sales', salary: 53000, specialization: 'Go' },
     //... D'autres enregistrements d'employés peuvent être ajoutés ici
 ];
 
 
 
 function displayEmployees() {
-    const totalEmployees = employees.map((employee, index) => `<p>${employee.id}: ${employee.name}: ${employee.name} - ${employee.department} - $${employee.salary}</p>`).join('');
+    const totalEmployees = employees.map((employee, index) => `<p>${employee.id}: ${employee.name}: ${employee.name} - ${employee.department} - $${employee.salary} - ${employee.specialization}</p>`).join('');
     document.getElementById('employeesDetails').innerHTML = totalEmployees;
 }
 
@@ -27,14 +27,24 @@ function calculateTotalSalaries() {
 
 function displayHREmployees() {
     const hrEmployees = employees.filter(employee => employee.department === 'HR');
-    const hrEmployeesDisplay = hrEmployees.map((employee, index) => `<p>${employee.id}: ${employee.name}: ${employee.name} - ${employee.department} - $${employee.salary}</p>`).join('');
+    const hrEmployeesDisplay = hrEmployees.map((employee, index) => `<p>${employee.id}: ${employee.name}: ${employee.name} - ${employee.department} - $${employee.salary} - ${employee.specialization}</p>`).join('');
     document.getElementById('employeesDetails').innerHTML = hrEmployeesDisplay;
 }
 
 function findEmployeeById(employeeId) {
     const foundEmployee = employees.find(employee => employee.id === employeeId);
     if(foundEmployee) {
-        document.getElementById('employeesDetails').innerHTML = `<p>${foundEmployee.id}: ${foundEmployee.name}: ${foundEmployee.name} - ${foundEmployee.department} - $${foundEmployee.salary}</p>`;
+        document.getElementById('employeesDetails').innerHTML = `<p>${foundEmployee.id}: ${foundEmployee.name}: ${foundEmployee.name} - ${foundEmployee.department} - $${foundEmployee.salary} - ${foundEmployee.specialization}</p>`;
+    } else {
+        document.getElementById('employeesDetails').innerHTML = 'aucun employe n\'a ete trouve avec cet ID';
+    }
+}
+
+function findEmployeeBySpecialization(EmployeeSpecialization) {
+    const foundEmployeeJs = employees.find(employee => employee.specialization === EmployeeSpecialization);
+    console.log(foundEmployeeJs);
+    if(foundEmployeeJs) {
+        document.getElementById('employeesDetails').innerHTML = `<p>${foundEmployeeJs.id}: ${foundEmployeeJs.name}: ${foundEmployeeJs.name} - ${foundEmployeeJs.department} - $${foundEmployeeJs.salary} - ${foundEmployeeJs.specialization}</p>`;
     } else {
         document.getElementById('employeesDetails').innerHTML = 'aucun employe n\'a ete trouve avec cet ID';
     }
